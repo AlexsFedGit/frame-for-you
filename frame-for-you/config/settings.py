7b0831b4e4ef94181366c7e881ca0d6584761e90
Alex Fed <AlexFedGit@Gmail.com>
2023-04-02 21:16:08 +0300

    Changed config-release/debug loading

--- frame-for-you/config/settings.py
+++ frame-for-you/config/settings.py
@@ -17,7 +17,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(os.path.join(BASE_DIR, 'config.json')) as config_file:
+if os.path.exists(os.path.join(BASE_DIR, 'config.json')):
+    config_path = os.path.join(BASE_DIR, 'config.json')
+else:
+    config_path = os.path.join(BASE_DIR, 'config-debug.json')
+
+with open(config_path) as config_file:
     config = json.load(config_file)
 
 # Quick-start development settings - unsuitable for production
